1bf06ff86e45972a00dc47c609c1618df1f44ad6
Esther Turin <turinesther@gmail.com>
2022-04-03 00:45:32 -0400

    Add project files.

---
+++ AjaxPractice/Controllers/PeopleController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AjaxPractice.Data;
+
+namespace AjaxDemo.Controllers
+{
+    public class PeopleController : Controller
+    {
+
+        private string _connectionString =
+            @"Data Source=.\sqlexpress;Initial Catalog=MyFirstDataBase;Integrated Security=true;";
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetAll()
+        {
+            var repo = new PeopleDb(_connectionString);
+            List<Person> people = repo.GetAll();
+            return Json(people);
+        }
+
+        [HttpPost]
+        public IActionResult AddPerson(Person person)
+        {
+            var repo = new PeopleDb(_connectionString);
+            repo.AddPerson(person);
+            return Json(person);
+        }
+        [HttpPost]
+        public IActionResult Edit(Person person)
+        {
+            var repo = new PeopleDb(_connectionString);
+            repo.EditPerson(person);
+            return Json(person);
+        }
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var repo = new PeopleDb(_connectionString);
+            repo.DeletePerson(id);
+            return Json(id);
+        }
+    }
+}
---
+++ AjaxProject.Data/PeopleDB.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AjaxPractice.Data
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class PeopleDb
+    {
+        private string _connectionString;
+
+        public PeopleDb(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<Person> GetAll()
+        {
+            using var conn = new SqlConnection(_connectionString);
+            using var cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT * FROM People";
+            conn.Open();
+            var reader = cmd.ExecuteReader();
+            List<Person> ppl = new();
+            while (reader.Read())
+            {
+                ppl.Add(new Person
+                {
+                    Id = (int)reader["Id"],
+                    FirstName = (string)reader["FirstName"],
+                    LastName = (string)reader["LastName"],
+                    Age = (int)reader["Age"],
+
+                });
+            }
+
+            return ppl;
+        }
+
+        public void AddPerson(Person person)
+        {
+            using var conn = new SqlConnection(_connectionString);
+            using var cmd = conn.CreateCommand();
+            cmd.CommandText = "INSERT INTO People (FirstName, LastName, Age) " +
+                "VALUES(@f, @l, @a) SELECT SCOPE_IDENTITY()";
+            cmd.Parameters.AddWithValue("@f", person.FirstName);
+            cmd.Parameters.AddWithValue("@l", person.LastName);
+            cmd.Parameters.AddWithValue("@a", person.Age);
+            conn.Open();
+            person.Id = (int)(decimal)cmd.ExecuteScalar();
+        }
+        public void EditPerson(Person person)
+        {
+            using var conn = new SqlConnection(_connectionString);
+            using var cmd = conn.CreateCommand();
+            cmd.CommandText = @"UPDATE People
+                SET FirstName = @f, LastName = @l, Age = @a 
+Where id =@i";
+               
+            cmd.Parameters.AddWithValue("@f", person.FirstName);
+            cmd.Parameters.AddWithValue("@l", person.LastName);
+            cmd.Parameters.AddWithValue("@a", person.Age);
+            cmd.Parameters.AddWithValue("@i", person.Id);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+          
+        }
+        public void DeletePerson(int id)
+        {
+            using var conn = new SqlConnection(_connectionString);
+            using var cmd = conn.CreateCommand();
+            cmd.CommandText = @"Delete from People
+               Where id =@i";
+            cmd.Parameters.AddWithValue("@i", id);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+     
+        }
+    }
+}
